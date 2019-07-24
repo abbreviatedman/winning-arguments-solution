388a16c8e7ab02575be57d62ef0e04737c513ee2
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-24 13:02:56 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const seanBeanMovies = [
+  'The Martian',
+  'Percy Jackson & the Olympians: The Lightning Thief',
+  'Far North',
+  'The Island',
+  'National Treasure',
+  'Troy',
+  'The Lord of The Rings: The Return of the King',
+  'Equilibrium',
+  'The Lord of The Rings: The Two Towers',
+  'The Lord of The Rings: The Fellowship of the Ring',
+  'Anna Karenina',
+  'Goldeneye'
+];
+
+const seanBeanShows = [
+  'Curfew',
+  'Medici',
+  'Wasted',
+  'Missing',
+  'Legends',
+  'Game of Thrones',
+  'Crusoe',
+  'Clarissa',
+  'The Practice',
+]
